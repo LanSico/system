64fbc31eb2b8d2b2af4395aa0579a22d61414758
djj <291909157@qq.com>
2018-08-26 19:59:08 +0800

    database

--- system/s/s/settings.py
+++ system/s/s/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 's.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sign_system',
+        'NAME': 'cs_sys',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
