c132156641e6e33399724d985c05f86bbdb18a23
andy <andy@gmail.com>
2018-08-26 21:29:52 +0800

    add some fields and delete some error fields

--- system/s/app/migrations/models.py
+++ system/s/app/migrations/models.py
@@ -4,30 +4,45 @@ from django.db import models
 
 
 class Student(models.Model):
-    User_id = models.AutoField(primary_key=True, auto_now_add=True)
-    UserName = models.CharField(max_length=20, auto_now_add=True)
-    PassWord = models.CharField(max_length=20, auto_now_add=True)
-    Email = models.CharField(max_length=30, auto_now_add=True)
-    Tag = models.TextField(max_length=30, null=True, auto_now_add=True)
+    User_id = models.AutoField(primary_key=True)
+
+    UserName = models.CharField(max_length=20)
+    Uid_LastChangeTime = models.DateTimeField(auto_now_add=True)
+
+    PassWord = models.CharField(max_length=20)
+    Pwd_LastChangeTime = models.DateTimeField(auto_now_add=True)
+
+    Email = models.CharField(max_length=30)
+
+    Tag = models.TextField(max_length=30, null=True)
 
     def __str__(self):
         return self.UserName
 
 
 class ClubAdmin(Student):
-    Club = models.charField(max_length=30, auto_now_add=True)
+    Club = models.charField(max_length=30)
 
     def __str__(self):
         return self.UserName
 
 
 class Activity(models.Model):
-    Act_id = models.AutoField(primary_key=True, auto_now_add=True)
-    Title = models.CharField(max_length=100, auto_now_add=True)
-    Content = models.charField(max_length=400, auto_now_add=True)
-    ReleaseTime = models.DateTimeField(auto_created=True, auto_now_add=True)
-    StartTime = models.DateTimeField(auto_now_add=True)
-    EndTime = models.DateTimeField(auto_now_add=True)
+    Act_id = models.AutoField(primary_key=True)
+
+    Title = models.CharField(max_length=100)
+    Title_LastChangeTime = models.DateTimeField(auto_now_add=True)
+    
+    Content = models.charField(max_length=400)
+    Content_LastChangeTime = models.DateTimeField(auto_now_add=True)
+
+    StartTime = models.DateTimeField()
+    St_LastChangeTime = models.DateTimeField(auto_now_add=True)
+
+    EndTime = models.DateTimeField()
+    Et_LastChangeTime = models.DateTimeField(auto_now_add=True)
+
+    ReleaseTime = models.DateTimeField(auto_created=True)
 
     def __str__(self):
         return self.Title
