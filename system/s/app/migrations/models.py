ceca3bc1c9d49ba868d61789b9f87190ace20845
andy <andy@gmail.com>
2018-08-26 15:09:40 +0800

    add file

---
+++ system/s/app/migrations/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    UserName = models.CharField(max_length=20)
+    PassWord = models.CharField(max_length=20)
+    Email = models.CharField(max_length=30)
+    Tag = models.TextField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.UserName
+
+
+class ClubAdmin(Student):
+    Club = models.charField(max_length=30)
+
+    def __str__(self):
+        return self.UserName
+
+
+class Activity(models.Model):
+    Title = models.CharField(max_length=100)
+    Content = models.charField()
+    ReleaseTime = models.TimeField(auto_created=True)
+    StartTime = models.TimeField()
+    EndTime = models.TimeField()
