508eecf2d62c7fcd6f25ccbd37539dce4c60a0f3
andy <andy@gmail.com>
2018-08-26 22:08:29 +0800

    delete some fields

--- system/s/app/migrations/models.py
+++ system/s/app/migrations/models.py
@@ -7,12 +7,9 @@ class Student(models.Model):
     User_id = models.AutoField(primary_key=True)
 
     UserName = models.CharField(max_length=20)
-    Uid_LastChangeTime = models.DateTimeField(auto_now_add=True)
-
     PassWord = models.CharField(max_length=20)
-    Pwd_LastChangeTime = models.DateTimeField(auto_now_add=True)
-
     Email = models.CharField(max_length=30)
+    LastChangeTime = models.DateTimeField(auto_now_add=True)
 
     Tag = models.TextField(max_length=30, null=True)
 
@@ -31,18 +28,13 @@ class Activity(models.Model):
     Act_id = models.AutoField(primary_key=True)
 
     Title = models.CharField(max_length=100)
-    Title_LastChangeTime = models.DateTimeField(auto_now_add=True)
-    
     Content = models.charField(max_length=400)
-    Content_LastChangeTime = models.DateTimeField(auto_now_add=True)
 
     StartTime = models.DateTimeField()
-    St_LastChangeTime = models.DateTimeField(auto_now_add=True)
-
     EndTime = models.DateTimeField()
-    Et_LastChangeTime = models.DateTimeField(auto_now_add=True)
-
     ReleaseTime = models.DateTimeField(auto_created=True)
+
+    LastChangeTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.Title
