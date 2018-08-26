4d2573463e5c0703749799ea7d006e7685ad7143
andy <andy@gmail.com>
2018-08-26 21:05:04 +0800

    add modles fields

--- system/s/app/migrations/models.py
+++ system/s/app/migrations/models.py
@@ -4,30 +4,30 @@ from django.db import models
 
 
 class Student(models.Model):
-    User_id = models.AutoField(primary_key=True)
-    UserName = models.CharField(max_length=20)
-    PassWord = models.CharField(max_length=20)
-    Email = models.CharField(max_length=30)
-    Tag = models.TextField(max_length=30, null=True)
+    User_id = models.AutoField(primary_key=True, auto_now_add=True)
+    UserName = models.CharField(max_length=20, auto_now_add=True)
+    PassWord = models.CharField(max_length=20, auto_now_add=True)
+    Email = models.CharField(max_length=30, auto_now_add=True)
+    Tag = models.TextField(max_length=30, null=True, auto_now_add=True)
 
     def __str__(self):
         return self.UserName
 
 
 class ClubAdmin(Student):
-    Club = models.charField(max_length=30)
+    Club = models.charField(max_length=30, auto_now_add=True)
 
     def __str__(self):
         return self.UserName
 
 
 class Activity(models.Model):
-    Act_id = models.AutoField(primary_key=True)
-    Title = models.CharField(max_length=100)
-    Content = models.charField()
-    ReleaseTime = models.TimeField(auto_created=True)
-    StartTime = models.TimeField()
-    EndTime = models.TimeField()
+    Act_id = models.AutoField(primary_key=True, auto_now_add=True)
+    Title = models.CharField(max_length=100, auto_now_add=True)
+    Content = models.charField(max_length=400, auto_now_add=True)
+    ReleaseTime = models.DateTimeField(auto_created=True, auto_now_add=True)
+    StartTime = models.DateTimeField(auto_now_add=True)
+    EndTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.Title
