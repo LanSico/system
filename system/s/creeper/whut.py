3284c6fd2b1b268d59e1e332c598e199ba71034f
andy <andy@gmail.com>
2018-08-28 14:35:46 +0800

    update whut creeper

--- system/s/creeper/whut.py
+++ system/s/creeper/whut.py
@@ -34,20 +34,17 @@ def login(usn,pwd):
 	r=s.post('http://sso.jwc.whut.edu.cn/Certification//login.do',data = data,headers = header)
 	
 def getGrade():
-	'''header={
-		'Host': '202.114.90.180',
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		'Accept-Language': 'zh,en-US;q=0.7,en;q=0.3',
-		'Accept-Encoding': 'gzip, deflate',
-		'Referer': 'http://sso.jwc.whut.edu.cn/Certification/login.do',
-		'Connection': 'keep-alive',
-		'Upgrade-Insecure-Requests': '1'
-	}'''
+	header={
+		'Referer':'http://sso.jwc.whut.edu.cn/Certification//login.do',
+		'Upgrade-Insecure-Requests':'1',
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'
+	}
+	r=s.get('http://202.114.90.180/Score',headers=header)
+	#print(r.headers)
 	
 	r=s.get('http://202.114.90.180/Score/')
-	#r=s.get('http://202.114.90.180/Score/')
-	header={
+	print(r.headers)
+	#header={
 		'Host': '202.114.90.180',
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
 		'Accept': '*/*',
@@ -58,28 +55,6 @@ def getGrade():
 	}
 	r=s.get('http://202.114.90.180/Score/djksList.do?_='+str(int(time()*1000)),headers=header)
 	dataHandle(r.text)
-		
-def getGrade():
-	header={
-		'Referer':'http://sso.jwc.whut.edu.cn/Certification//login.do',
-		'Upgrade-Insecure-Requests':'1',
-		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
-	}
-	r=s.get('http://202.114.90.180/Score',headers=header)
-	print(r.headers)
-	header={
-		'Host': '202.114.90.180',
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-		'Accept-Language': 'zh-CN,zh;q=0.9',
-		'Accept-Encoding': 'gzip, deflate',
-		'X-Requested-With': 'XMLHttpRequest',
-		'Connection': 'keep-alive',
-		'Upgrade-Insecure-Requests':'1',
-		'Referer':'http://sso.jwc.whut.edu.cn/Certification//login.do'
-	}
-	r=s.get('http://202.114.90.180/Score/')
-	print(r.headers)
 
 def dataHandle(content):
 	tagRegx=re.compile('<th .*?/th>')
