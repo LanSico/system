32e1f6980d7027ad9736bc88e4b53aebf2790a68
wuliaotc <372072553@qq.com>
2019-03-04 15:10:39 +0800

    fixed register

--- system/s/app/views_1.py
+++ system/s/app/views_1.py
@@ -18,25 +18,30 @@ def user_register(request):
 		u_name = request.POST.get("username")
 		u_psd = request.POST.get("password")
 		u_email = request.POST.get("email")
-		if(checkData(u_name, u_psd, u_email)):  # 检查格式
+		print(u_name)
+		print(u_psd)
+		print(u_email)
+		if checkData(u_name, u_psd, u_email):  # 检查格式
 			try:
 				Student.objects.get(UserName=u_name)  # 用户已存在
-				return HttpResponse('{"status_code":"400","json":{"error":"用户已存在"}}')
- 			except Content.DoesNotExist:
+			except:
 				try:
 					Student.objects.get(Email=u_email)
-					return HttpResponse('{"status_code":"400","json":{"error":"该邮箱已被注册"}}')
-				except Content.DoesNotExist:
+				except:
 					Student.objects.create(UserName=u_name, Email=u_email, PassWord=u_psd)
 					return HttpResponse('{"status_code":200,"json":{"status":true,"message":"账号注册成功"}}')
+				else:
+					return HttpResponse('{"status_code":"400","json":{"error":"该邮箱已被注册"}}')
+			else:
+				return HttpResponse('{"status_code":"400","json":{"error":"用户已存在"}}')
 		else:  # 非法访问
+			print("test")
 			return HttpResponse('{"status_code":"403","json":{"error":"非法请求"}}')
 	else:  # 非法访问
-		return HttpResponse('{"status_code":"403","json":{"error":"非法请求"}}')
+		return HttpResponse('{"status_code":"403","json":{"error":"非法请求GET"}}')
 
 
 def checkData(u_name, u_psd, u_email):
-	return True
 	if u_name and u_psd and u_email:  # 都不为空
-		pass
+		return True
 		#if re.match(r"[]",,)
